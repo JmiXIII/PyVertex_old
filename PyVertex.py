69b093ad760836ecc75fa83c5b6fe9281537b39e
JmiXIII <sylvn.page@gmail.com>
2016-09-21 22:09:34 +0200

    Update PyVertex.py
    PEP8 corrections

--- PyVertex.py
+++ PyVertex.py
@@ -7,37 +7,36 @@ Created on Fri Feb 26 12:51:27 2016
 
 import os
 import glob
-import re
 import xlwings as xw
 import numpy as np
 import pandas as pd
 
 
-color_OK = (208,254,182)
-color_NOK = (236,181,167)
-path=os.getcwd()
-path="G:/01-SUIVI PROCESSUS/GMM/Micro-Vu Vertex/Schrader/43173-820"
-fcsv=glob.glob(path+"/CSV Data/*.txt")[0] #File containing CSV
+color_OK = (208, 254, 182)
+color_NOK = (236, 181, 167)
+path = os.getcwd()
+path = "G:/01-SUIVI PROCESSUS/GMM/Micro-Vu Vertex/Schrader/43173-820"
+fcsv = glob.glob(path+"/CSV Data/*.txt")[0]  # File containing CSV
 os.system("pause")
-fxls=glob.glob(path+"/Layout/*.xlsx")[0] #xlsx file layout for report
+fxls = glob.glob(path+"/Layout/*.xlsx")[0]  # xlsx file layout for report
 os.system("pause")
-#print(fcsv,fxls)
-#os.system("pause")
+# print(fcsv,fxls)
+# os.system("pause")
 
-def file2df(fname):   
+
+def file2df(fname):
     """CSV import function"""
-    
-    data = pd.read_csv(fname,header=None,
-                 sep=None,
-                 index_col=False,)
+    data = pd.read_csv(fname, header=None,
+                       sep=None,
+                       index_col=False)
     return data
 
 wb = xw.Workbook(fxls)
-data=file2df(fcsv)
+data = file2df(fcsv)
 for index, row in data.iterrows():
-#    os.system("pause")
-    loc=data[0][index]
-    value=data[1][index]
+    # os.system("pause")
+    loc = data[0][index]
+    value = data[1][index]
     if np.isnan(data[4][index]):
         if np.isnan(data[3][index]):
             color = color_OK
@@ -46,11 +45,9 @@ for index, row in data.iterrows():
     elif np.isnan(data[6][index]):
                 color = color_OK
     else:
-        color = color_NOK       
-    #color = (255,255,255)
-#    print(loc,value)
+        color = color_NOK
     xw.Range(str(loc)).value = value
     xw.Range(str(loc)).color = color
 wb.xl_workbook.PrintOut()
-#os.system("pause")
+# os.system("pause")
 wb.close()
